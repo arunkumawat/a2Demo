176f588034f474fc4dc8097218c3004d6dfaa7fd
Arun Kumawat <i.arunkk@gmail.com>
2018-05-18 19:30:42 +0530

    Update registrationReport.component.ts

--- app/admin/registrationReport.component.ts
+++ app/admin/registrationReport.component.ts
@@ -26,30 +26,10 @@ export class RegistrationReportComponent extends Util {
         this.rolesArr = this.roleS.getUserRoles();
         this.loggedInUserName = roleS.getUserFullName();
 
-        this.fetchClients();
-    }
-    fetchClients() {
-        this.busyB = this.ps.get('Users?filter.roleName=client').subscribe(posts => {
-            this.clientList = posts.data;
-        });
-    }
-    reRegister(userId: number) {
-        this.busyA = this.ps.put('Users/Re-register/' + userId, {}).subscribe(posts => {
-            if (!posts.isSuccess) {
-                this.toastr.error(posts.message, 'Important');
-                return;
-            } else {
-                this.toastr.success("Client successfully re registered", 'Success');
-                this.fetchClients();
-            }
-        });
     }
 
-    sortByRegisteredBy() {
-        this.isDesc = !this.isDesc; //change the direction  
-        this.column = 'registeredBy';  
-        let direction = this.isDesc ? 1 : -1;
 
+    sortByBy() {
         this.clientList.sort(function (a, b) {
             if (a.registeredBy.firstName.toLowerCase() < b.registeredBy.firstName.toLowerCase()) {
                 return -1 * direction;
